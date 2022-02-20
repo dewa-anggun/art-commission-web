1ea5a429ab70945feb5119706ad60d3cc137c0ae
dewa-anggun <sono00owicak@gmail.com>
2022-02-20 21:32:08 +0700

    adding javascript alert and fixing some CSS

---
+++ assets/scripts/index.js
@@ -0,0 +1,8 @@
+function confirmOrder() {
+    const contact = confirm("Anda akan menghubungi Dewa Anggun");
+    if (contact) {
+        window.location = "https://api.whatsapp.com/send?phone=+628156958491";
+    } else {
+        alert("Terima kasih. Mungkin lain waktu :)")
+    }
+}
